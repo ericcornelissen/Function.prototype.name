7fe6fc3ac677b9e8f5f73661e15c7c8e5f453743
Jordan Harband <ljharb@gmail.com>
2026-06-11 14:33:02 -0700

    [Refactor] use `is-document.all`

--- implementation.js
+++ implementation.js
@@ -5,10 +5,10 @@ var hasOwn = require('hasown');
 var functionsHaveNames = require('functions-have-names')();
 var $TypeError = require('es-errors/type');
 var callBound = require('call-bound');
+var isDDA = require('is-document.all');
 
 var $functionToString = callBound('Function.prototype.toString');
 var $stringMatch = callBound('String.prototype.match');
-var toStr = callBound('Object.prototype.toString');
 
 var classRegex = /^class /;
 
@@ -29,37 +29,7 @@ var isClass = function isClassConstructor(fn) {
 
 var regex = /\s*function\s+([^(\s]*)\s*/;
 
-var isIE68 = !(0 in [,]); // eslint-disable-line no-sparse-arrays
-
-var objectClass = '[object Object]';
-var ddaClass = '[object HTMLAllCollection]';
-
 var functionProto = Function.prototype;
-
-/** @type {(() => false) | ((value: unknown) => value is HTMLAllCollection)} */
-var isDDA = function isDocumentDotAll() {
-	return /** @type {const} */ (false);
-};
-if (typeof document === 'object') {
-	// Firefox 3 canonicalizes DDA to undefined when it's not accessed directly
-	var all = document.all;
-	if (toStr(all) === toStr(document.all)) {
-		isDDA = /** @type {(value: unknown) => value is HTMLAllCollection} */ function isDocumentDotAll(value) {
-			/* globals document: false */
-			// in IE 6-8, typeof document.all is "object" and it's truthy
-			if ((isIE68 || !value) && (typeof value === 'undefined' || typeof value === 'object')) {
-				try {
-					// @ts-expect-error nullish will throw
-					var str = toStr(value);
-					// IE 6-8 uses `objectClass`
-					return (str === ddaClass || str === objectClass)
-						&& /** @type {Function} */ (value)('') == null;
-				} catch (e) { /**/ }
-			}
-			return false;
-		};
-	}
-}
 
 /** @type {import('./implementation')} */
 module.exports = function getName() {
